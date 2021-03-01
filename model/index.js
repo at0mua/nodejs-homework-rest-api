87d6aac532f852a7613be420c9a5a8e42b9a6aa9
At0mUA <zatom89@gmail.com>
2021-03-01 16:19:07 +0200

    added small fix to contact controller

--- model/index.js
+++ model/index.js
@@ -6,10 +6,9 @@ const contactsPath = path.resolve('db', 'contacts.json')
 
 const listContacts = async (_req, res, next) => {
   try {
-    await fs.readFile(contactsPath, 'utf-8').then(data => {
-      const contacts = JSON.parse(data)
-      res.status(HttpCode.OK).json(contacts)
-    })
+    const contacts = await fs.readFile(contactsPath, 'utf-8')
+    const parsedContacts = JSON.parse(contacts)
+    res.status(HttpCode.OK).json(parsedContacts)
   } catch (err) {
     next(err)
   }
@@ -18,19 +17,18 @@ const listContacts = async (_req, res, next) => {
 const getContactById = async (req, res, next) => {
   try {
     const { contactId } = req.params
-    await fs.readFile(contactsPath, 'utf-8').then(data => {
-      const parsedContacts = JSON.parse(data)
-      const contactById = parsedContacts.find(
-        contact => contact.id === Number(contactId),
-      )
-      if (contactById) {
-        return res.status(HttpCode.OK).json(contactById)
-      } else {
-        return res
-          .status(HttpCode.NOT_FOUND)
-          .json({ message: 'Contact not Found' })
-      }
-    })
+    const contacts = await fs.readFile(contactsPath, 'utf-8')
+    const parsedContacts = JSON.parse(contacts)
+    const contactById = parsedContacts.find(
+      contact => contact.id === Number(contactId),
+    )
+    if (contactById) {
+      return res.status(HttpCode.OK).json(contactById)
+    } else {
+      return res
+        .status(HttpCode.NOT_FOUND)
+        .json({ message: 'Contact not Found' })
+    }
   } catch (err) {
     next(err)
   }
@@ -39,22 +37,20 @@ const getContactById = async (req, res, next) => {
 const removeContact = async (req, res, next) => {
   try {
     const { contactId } = req.params
-    await fs.readFile(contactsPath, 'utf-8').then(data => {
-      const parsedContacts = JSON.parse(data)
-      const contactIndex = parsedContacts.findIndex(
-        contact => contact.id === Number(contactId),
-      )
-      if (contactIndex !== -1) {
-        parsedContacts.splice(contactIndex, 1)
-        fs.writeFile(contactsPath, JSON.stringify(parsedContacts, null, 2))
-        return res.status(HttpCode.OK).json({ message: 'Contact deleted' })
-        // return res.status(HttpCode.OK).json(filterContacts)
-      } else {
-        return res
-          .status(HttpCode.NOT_FOUND)
-          .json({ message: 'Contact not Found' })
-      }
-    })
+    const contacts = await fs.readFile(contactsPath, 'utf-8')
+    const parsedContacts = JSON.parse(contacts)
+    const contactIndex = parsedContacts.findIndex(
+      contact => contact.id === Number(contactId),
+    )
+    if (contactIndex !== -1) {
+      parsedContacts.splice(contactIndex, 1)
+      fs.writeFile(contactsPath, JSON.stringify(parsedContacts, null, 2))
+      return res.status(HttpCode.OK).json({ message: 'Contact deleted' })
+    } else {
+      return res
+        .status(HttpCode.NOT_FOUND)
+        .json({ message: 'Contact not Found' })
+    }
   } catch (err) {
     next(err)
   }
@@ -63,16 +59,14 @@ const removeContact = async (req, res, next) => {
 const addContact = async (req, res, next) => {
   const newContact = req.body
   try {
-    await fs.readFile(contactsPath, 'utf-8').then(data => {
-      const parsedContacts = JSON.parse(data)
-      const id =
-        parsedContacts.length > 0 ? [...parsedContacts].pop().id + 1 : 1
-      fs.writeFile(
-        contactsPath,
-        JSON.stringify([...parsedContacts, { id, ...newContact }], null, 2),
-      )
-      res.status(HttpCode.CREATED).json(newContact)
-    })
+    const contacts = await fs.readFile(contactsPath, 'utf-8')
+    const parsedContacts = JSON.parse(contacts)
+    const id = parsedContacts.length > 0 ? [...parsedContacts].pop().id + 1 : 1
+    fs.writeFile(
+      contactsPath,
+      JSON.stringify([...parsedContacts, { id, ...newContact }], null, 2),
+    )
+    res.status(HttpCode.CREATED).json(newContact)
   } catch (err) {
     next(err)
   }
@@ -81,31 +75,30 @@ const addContact = async (req, res, next) => {
 const updateContact = async (req, res, next) => {
   try {
     const { contactId } = req.params
-    await fs.readFile(contactsPath, 'utf-8').then(data => {
-      const parsedContacts = JSON.parse(data)
-      const contactIndex = parsedContacts.findIndex(
-        contact => contact.id === Number(contactId),
-      )
-      if (contactIndex !== -1) {
-        const updatedContact = {
-          ...parsedContacts[contactIndex],
-          ...req.body,
-        }
-        const updatedContacts = [
-          ...parsedContacts.slice(0, contactIndex),
-          updatedContact,
-          ...parsedContacts.slice(contactIndex + 1),
-        ]
-        fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2))
-        return res
-          .status(HttpCode.OK)
-          .json({ message: 'Contact updated successfully' })
-      } else {
-        return res
-          .status(HttpCode.NOT_FOUND)
-          .json({ message: 'Contact not Found' })
+    const contacts = await fs.readFile(contactsPath, 'utf-8')
+    const parsedContacts = JSON.parse(contacts)
+    const contactIndex = parsedContacts.findIndex(
+      contact => contact.id === Number(contactId),
+    )
+    if (contactIndex !== -1) {
+      const updatedContact = {
+        ...parsedContacts[contactIndex],
+        ...req.body,
       }
-    })
+      const updatedContacts = [
+        ...parsedContacts.slice(0, contactIndex),
+        updatedContact,
+        ...parsedContacts.slice(contactIndex + 1),
+      ]
+      fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2))
+      return res
+        .status(HttpCode.OK)
+        .json({ message: 'Contact updated successfully' })
+    } else {
+      return res
+        .status(HttpCode.NOT_FOUND)
+        .json({ message: 'Contact not Found' })
+    }
   } catch (err) {
     next(err)
   }
